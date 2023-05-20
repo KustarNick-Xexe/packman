9f687e3355a71b621812743b7403964f3e025aa3
Salikh Valitov <salikhcomhd@gmail.com>
2023-05-20 08:51:26 +0500

    сетка

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -10,16 +10,20 @@ const Box = ({ position, dimensions, isFrigile }) => {
     const edges = new EdgesGeometry(new BoxGeometry(...dimensions));
     const line = new LineSegments(edges, new LineBasicMaterial({ color: 0x000000 }));
 
+    // Compute the offset for each box
+    const offset = dimensions.map(dimension => dimension / 2);
+
     return (
         <group>
-            <mesh position={position} ref={boxRef}>
+            <mesh position={[position[0] + offset[0], position[1] + offset[1], position[2] + offset[2]]} ref={boxRef}>
                 <boxGeometry args={dimensions} />
                 <meshBasicMaterial color={isFrigile ? '#03d9ff' : '#0370ff'} />
             </mesh>
-            <primitive object={line} position={position} />
+            <primitive object={line} position={[position[0] + offset[0], position[1] + offset[1], position[2] + offset[2]]} />
         </group>
     );
 };
+
 
 const Controls = () => {
     const orbitRef = useRef();
@@ -42,20 +46,30 @@ const Scene = ({ boxes, containerDimensions }) => {
     const arrowY = new ArrowHelper(new Vector3(0, 1, 0), new Vector3(0, 0, 0), containerDimensions[1], 'green');
     const arrowZ = new ArrowHelper(new Vector3(0, 0, 1), new Vector3(0, 0, 0), containerDimensions[2], 'blue');
 
+    // Compute the offset for the container
+    const containerOffset = containerDimensions.map(dimension => dimension / 2);
+
+    // Set the size of the grid to be the same as the container's base dimensions
+    const gridSize = [containerDimensions[0] + 5, containerDimensions[2] + 5];
+
+    // Position the grid in the bottom quarter of the container
+    const gridPosition = [containerOffset[0], 0, containerOffset[2]];
+
     return (
         <Canvas>
             <ambientLight />
             <Controls />
-            <primitive object={containerLine} />
+            <primitive object={containerLine} position={containerOffset} />
             <primitive object={arrowX} />
             <primitive object={arrowY} />
             <primitive object={arrowZ} />
-            <gridHelper args={[14, 10]} />
+            <gridHelper args={gridSize} position={gridPosition} />
             {boxes.map((box, i) => (
                 <Box key={i} position={[box[0], box[1], box[2]]} dimensions={[box[3], box[4], box[5]]} isFrigile={box[6]} />
             ))}
         </Canvas>
     );
 };
+
 
 export default Scene;
