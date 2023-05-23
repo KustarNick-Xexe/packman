d4da3a740c65112ceec225d123bef8998273a592
Salikh Valitov <salikhcomhd@gmail.com>
2023-05-23 19:43:05 +0500

    небольшие изменения

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,8 +1,15 @@
 import Tittle from './Tittle';
 import Field from './Field';
 import Button from './Button';
+import axios from 'axios';
 
 const Settings = () => {
+
+    const handleClick = async () => {
+      const response = await axios.get('http//localhost:3000/pack');
+      const result = await response.data;
+      console.log(result);
+    };
 
     return (
       <div className='bg-white w-72 shadow px-4 py-4 m-4 rounded-md'>
@@ -11,26 +18,36 @@ const Settings = () => {
           description={'Введите параметры, которые соответствуют данным депо'}
         />
         <Field 
-          label={'Количество ТС'} 
+          label={'Средняя скорость'} 
+          name={'кол-во'} 
+          placeholder={'Введите скорость'}
+        />
+        <Field 
+          label={'Расход топлива на 100км'} 
+          name={'кол-во'} 
+          placeholder={'Введите расход'}
+        />
+        <Field 
+          label={'Стоимость литра бензина'} 
+          name={'кол-во'} 
+          placeholder={'Введите стоимость'}
+        />
+        <Field 
+          label={'Время на разгрузку товара'} 
+          name={'кол-во'} 
+          placeholder={'Введите время'}
+        />
+        <Field 
+          label={'Штраф по временным окнам'} 
+          name={'кол-во'} 
+          placeholder={'Введите штраф'}
+        />
+         <Field 
+          label={'Количество машин в автопарке'} 
           name={'кол-во'} 
           placeholder={'Введите количество'}
         />
-        <Field 
-          label={'Грузоподъемность ТС'} 
-          name={'кол-во'} 
-          placeholder={'Введите массу'}
-        />
-        <Field 
-          label={'Количество популяций'} 
-          name={'кол-во'} 
-          placeholder={'Введите количество'}
-        />
-        <Field 
-          label={'Вероятность мутации'} 
-          name={'кол-во'} 
-          placeholder={'Введите вероятность'}
-        />
-        <Button text={'Отправить'} type={'submit'}/>
+        <Button text={'Отправить'} type={'submit'} onClick={ handleClick }/>
       </div>
     )
 }
